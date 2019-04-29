0c0d9424cd7cbd42fb7b4c2d181ed59845c2734e
c4arl0s <c.santiago.cruz@gmail.com>
2019-04-28 18:41:43 -0600

    add code

---
+++ UITextField-ObjectiveC/UITextField-ObjectiveC/ViewController.h
@@ -0,0 +1,17 @@
+//
+//  ViewController.h
+//  UITextField-ObjectiveC
+//
+//  Created by Carlos Santiago Cruz on 4/28/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+// add here the protocols if you need to show the information about them.
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UITextField-ObjectiveC/UITextField-ObjectiveC/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UITextField-ObjectiveC
+//
+//  Created by Carlos Santiago Cruz on 4/28/19.
+//  Copyright © 2019 Carlos Santiago Cruz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
